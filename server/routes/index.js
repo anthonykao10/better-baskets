8419a5ae6c6018dca89db0d143a4fcf281ef3c95
anthony <anthonykao10@gmail.com>
2019-10-24 18:29:08 -0700

    add initial routes, and spawn process to call track.py in dashboard route

---
+++ server/routes/index.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const { spawn } = require('child_process');
+
+router.get('/', (req, res) => {
+  res.send('homepage');
+});
+
+router.get('/dashboard', (req, res) => {
+  const track = spawn('python', ['lib/track.py']);
+  
+  track.on('close', (code) => {
+    console.log(`child process exited with code ${code}`);
+    res.redirect('/');
+  });
+});
+
+router.get('*', (req, res) => {
+  res.sendStatus(404);
+});
+
+module.exports = router;
