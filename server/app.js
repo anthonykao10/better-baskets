e1f3a7a9f9ca91fa1feb91a6425887253db73a81
anthonykao10 <anthonykao10@gmail.com>
2019-10-25 21:35:02 +0000

    update server port to use .env file

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.SERVER_PORT;
 
 app.use(require('./routes'));
 
