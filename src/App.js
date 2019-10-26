7db9771199c3c79605abf7e6df5af8ba74527a2d
Andrew Hui <andrew.j.hui@gmail.com>
2019-10-25 17:02:29 -0700

    s3 working with video file upload

--- src/App.js
+++ src/App.js
@@ -2,27 +2,35 @@ import React from 'react';
 import ReactMediaRecorder from "react-media-recorder";
 import logo from './logo.svg';
 import './App.css';
+import uploadVideo from './testUpload'
+import axios from 'axios'
 
 function App() {
+console.log(process.env)
+  const sendVideoToServer = function(videoBlob) {
+
+    axios.post(`http://localhost:3002`, {videoBlob
+    })
+    .then(() => {
+      console.log("Successful post to http://localhost:3002")
+    })
+    
+  }
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        
+
   <div>
     <ReactMediaRecorder
       video
+      blobPropertyBag={{type: "video/mp4"}}
+      whenStopped={(video) => {
+        uploadVideo(video) 
+        // sendVideoToServer(video)
+      }}
+
       render={({ status, startRecording, stopRecording, mediaBlob }) => (
         <div>
           <p>{status}</p>
