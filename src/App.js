1cb5f4bcaf0ee09be28cde30a407b652f9ceb08b
Sarah <sarah.mahov@gmail.com>
2019-10-28 02:30:34 +0000

    fix navbar error

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import './App.css';
-import uploadVideo from './videoUpload'
-import axios from 'axios'
+import {
+  BrowserRouter as Router,
+  Route
+} from 'react-router-dom'
 
 // all "route" imports
 import NewShotScreen from './components/NewShotScreen';
@@ -10,27 +12,11 @@ import DashboardScreen from './components/DashboardScreen';
 import ShotScreen from './components/ShotScreen';
 import SessionScreen from './components/SessionScreen';
 
-import {
-  BrowserRouter as Router,
-  Route
-} from 'react-router-dom'
-
 function App() {
-console.log(process.env)
-  const sendVideoToServer = function(videoBlob) {
-
-    axios.post(`http://localhost:3002`, {videoBlob
-    })
-    .then(() => {
-      console.log("Successful post to http://localhost:3002")
-    })
-    
-  }
-
 
   return (
     <div className="App">
-      <Router>
+      <Router> 
           <Route exact path="/">
             <LoginScreen />
           </Route>
@@ -56,30 +42,3 @@ console.log(process.env)
 }
 
 export default App;
-
-
-
-
-// <header className="App-header">
- 
-// <div>
-//  <ReactMediaRecorder
-//    video
-//    blobPropertyBag={{type: "video/mp4"}}
-//    whenStopped={(video) => {
-//      uploadVideo(video) 
-//      // sendVideoToServer(video)
-//    }}
-
-//    render={({ status, startRecording, stopRecording, mediaBlob }) => (
-//      <div>
-//        <p>{status}</p>
-//        <button onClick={startRecording}>Start Recording</button>
-//       <button onClick={stopRecording}>Stop Recording</button>
-//        <video src={mediaBlob} controls />
-//      </div>
-//    )}
-//  />
-// </div>
-
-//    </header>
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,8 +13,8 @@ export default function NavBar() {
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="mr-auto">
-        <Nav.Link><Link to="/dashboard">Home</Link></Nav.Link>
-        <Nav.Link><Link to="/new_shot">New Shot</Link></Nav.Link>
+        <Link to="/dashboard">Home</Link>
+        <Link to="/new_shot">New Shot</Link>
         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
           <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
           <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
