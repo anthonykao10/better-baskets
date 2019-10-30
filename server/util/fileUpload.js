d0d4276e1d81b34fe4644992ec71207f582066bc
Andrew <andrew.j.hui@gmail.com>
2019-10-30 22:54:05 +0000

    updating fileUpload file so that it video files can be publicly read

--- server/util/fileUpload.js
+++ server/util/fileUpload.js
@@ -2,8 +2,6 @@
 // Load the SDK and UUID
 const fs = require('fs');
 const AWS = require('aws-sdk');
-// env["AWS_KEY"]
-// Create an S3 client
 
 const awsConfig = new AWS.Config({
   credentials: {
@@ -19,38 +17,25 @@ var bucketName = 'betterbaskets'
 
 const fileUpload = function(video) {
   
-
-  console.log("fileUpload being hit")
-
-  // CANT FIND FILE PATH TO GET BUFFER SO IT CAN SEND TO S3
-  fs.readFileSync('../videos/newTestANTHONY.webm', (err, data) => {
+  fs.readFile(__dirname + '/../videoUploads/processedVideo.mp4', (err, data) => {
     if (err) {
       throw err;
     }
     else {
-      console.log(data, "DATA FROM readfile FS")
+
+      const params = {Bucket: bucketName, Key: 'processedVideo123.mp4', Body: data, ACL: 'public-read'};
+
+      s3.putObject(params, function(err, data) {
+        if (err) {
+        }
+        else {
+          console.log("Successfully uploaded data to " + bucketName + "/" + params.Key)
+        }
+      })
+
+
     }
   })
-
-  // fs.readFile('../videoUploads/processedVideo.mp4', (err, data) => {
-  //   if (err) {
-  //     throw err;
-  //   }
-  //   else {
-  //     console.log(data, "DATA FROM readfile FS")
-  //   }
-  // })
-
-  // var params = {Bucket: bucketName, Key: 'newTestANTHONY.webm', Body: video};
-
-  // s3.putObject(params, function(err, data) {
-  //   if (err) {
-  //     console.log(err)
-  //   } else {
-
-  //     console.log("Successfully uploaded data to " + bucketName + "/" + params.Key);
-  //   }
-  // });
 
 }
 
