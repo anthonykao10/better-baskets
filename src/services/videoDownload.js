a0771cb48282ecc84b321d790e99f09821d6fc8d
Andrew <andrew.j.hui@gmail.com>
2019-10-30 18:18:37 +0000

    updating s3 files

--- src/services/videoDownload.js
+++ src/services/videoDownload.js
@@ -15,8 +15,24 @@ const s3 = new AWS.S3(awsConfig);
 
 const bucketName = 'betterbaskets'
 
-const uploadVideo = function(video) {
-  
+const downloadVideo = function(video) {
+
+    var params = {
+    Bucket: bucketName, 
+    Key: 'newTestANTHONY.webm'
+  };
+
+  s3.getObject(params, function(err, data) {
+    if (err) {
+      console.log(err);
+      reject(err);
+    } else {
+      console.log("Successfully downloaded data " + bucketName + "/" + params.Key);
+      console.log('\ndata:', data);
+      resolve(data);
+    }
+  });
+
 
       // const params = {Bucket: bucketName, Key: 'newTestANTHONY.webm', Body: video};
 
@@ -31,4 +47,68 @@ const uploadVideo = function(video) {
 
     }
 
-export default uploadVideo
+export default downloadVideo
+
+
+// // Load the SDK and UUID
+// const fs = require('fs');
+// const AWS = require('aws-sdk');
+// // env["AWS_KEY"]
+// // Create an S3 client
+
+// // console.log('\nprocess.env:', process.env);
+
+// const awsConfig = new AWS.Config({
+//   credentials: {
+//     accessKeyId: process.env.AWS_KEY_ID,
+//     secretAccessKey: process.env.AWS_KEY_SECRET
+//   },
+//   region: 'us-west-1'
+// })
+
+// var s3 = new AWS.S3(awsConfig);
+
+// var bucketName = 'betterbaskets'
+
+// const fileDownload = function() {
+
+//   var params = {
+//     Bucket: bucketName, 
+//     Key: 'newTestANTHONY.webm'
+//   };
+
+
+//   return (() => {
+//     return new Promise((resolve, reject) => {
+//       s3.getObject(params, function(err, data) {
+//         if (err) {
+//           console.log(err);
+//           reject(err);
+//         } else {
+//           console.log("Successfully downloaded data " + bucketName + "/" + params.Key);
+//           console.log('\ndata:', data);
+//           resolve(data);
+//         }
+//       });
+//     });
+//   })()
+//     .  then((data) => {
+//       return new Promise((resolve, reject) => {
+//         fs.writeFile(`videos/newTestANTHONY.webm`, data.Body, (err) => {
+//           if (err) {
+//             console.log(err, "readFile");
+//             reject(err);
+//           }
+//           else {
+//             console.log("file saved");
+//             resolve();
+//           }
+//         });
+//       });
+//     })
+//     .catch(err => console.log(err));
+
+// }
+
+
+// module.exports = fileDownload;
