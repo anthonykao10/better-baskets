7d410683f674c13bbd0d108d932bd31c2fff25cf
anthony <anthonykao10@gmail.com>
2019-10-31 11:07:45 -0700

    remove driver code in helpers.py

--- server/lib/python/helpers.py
+++ server/lib/python/helpers.py
@@ -2,19 +2,11 @@ import numpy.polynomial.polynomial as poly
 import sympy as sp
 import math
 
-
-testArr = [ [79,381],[106,350],[141,326],[181,290],[236,316],[274,307],[321,314],[367,318],[411,324],[465,318],[515,300],[570,285],[628,274],[682,250],[741,245],[794,233],[848,236],[895,251],[947,255],[1000,252],[1033,316],[1069,361] ]
-
 def getXVals(coords):
   return map(lambda coord: coord[0], coords)
 
 def getYVals(coords, res_height):
   return map( lambda coord: res_height - coord[1], coords )
-
-xVals = getXVals(testArr)
-yVals = getYVals(testArr, 720)
-# print(xVals)
-# print(yVals)
 
 def getArc(x_vals, y_vals):
   """Returns array of polynomial coefficients.
@@ -26,9 +18,6 @@ def getArc(x_vals, y_vals):
   """
   if len(x_vals) > 0 and len(y_vals) > 0:
     return poly.polyfit(x_vals, y_vals, 2).tolist()
-
-arc = getArc(xVals, yVals)
-# print(arc)
 
 def f(x, c, b, a):
   """Returns the value of polynomial evaluated 
@@ -56,8 +45,6 @@ def getArcMax(c, b, a):
     max_x = sp.solve(fprime, x)
     return round( f(max_x[0], c, b, a), 2 )
 
-# print('getArcMax:', getArcMax( arc[2], arc[1], arc[0] ))
-
 def getArcAngle(c, b, a):
   """Returns angle of a shot.
   Parameters:
@@ -71,5 +58,3 @@ def getArcAngle(c, b, a):
   fprime = sp.diff( f(x, c, b, a) )
   if fprime.diff(x) < 0:
     return round( math.degrees(math.atan(fprime.args[0])), 2 )
-
-print('getArcAngle:', getArcAngle( arc[2], arc[1], arc[0] ))
