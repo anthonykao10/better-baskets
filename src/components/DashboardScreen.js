9d365271ed01674c467929726b5f8564a6f0f48d
Sarah <sarah.mahov@gmail.com>
2019-11-03 22:26:14 +0000

    add success field

--- src/components/DashboardScreen.js
+++ src/components/DashboardScreen.js
@@ -19,6 +19,7 @@ export default function DashboardScreen({userData, sessionData, shotData, addSes
         key={session.id}
         sessionID={session.id} 
         date={session.date}
+        shotData={shotData}
         />
       );
     }
--- src/components/SessionComponents/Session.js
+++ src/components/SessionComponents/Session.js
@@ -4,8 +4,32 @@ import {
 } from 'react-router-dom'
 import { Card } from 'react-bootstrap';
 import '../styles/Carousel.css';
+import { FileSystemCredentials } from "aws-sdk";
  
-export default function Session({sessionID, date}) {
+export default function Session({sessionID, date, shotData}) {
+
+  const shotsBySession = shotData.filter((item) => item.session_id === parseInt(sessionID));
+
+  const numberOfShots = shotsBySession.length;
+  
+  const successPercent = () => {
+    let successCount = 0;
+    const count = shotsBySession.length;
+    
+    shotsBySession.forEach((item) => {
+      if(item.success === true) {
+        successCount += 1;
+      }    
+    })
+
+    if(count === 0) {
+      return 'Success: No Shots Taken';
+    } else {
+      return "Success: " + successCount/count * 100 + "%";
+    }
+  }
+
+  const sessionSuccessPercent = successPercent();
 
   return (
     <div className="sessionCardDiv">
@@ -15,11 +39,15 @@ export default function Session({sessionID, date}) {
       <Link to={`/session/${sessionID}`}>Session Link</Link> */}
 
       <Card bg="light" style={{ width: '16rem' }} className="sessionCard">
-        <Card.Header>Session: {sessionID}</Card.Header>
+        <Card.Header>
+          <Link to={`/session/${sessionID}`}>Session: {sessionID}</Link>
+        </Card.Header>
         <Card.Body>
-          {/* <Card.Title>Session: {sessionID}</Card.Title> */}
+          <Card.Title>Date: {date}</Card.Title>
           <Card.Text>
-          <Link to={`/session/${sessionID}`}>Session Link</Link>
+          
+          <p>Number of Shots: {numberOfShots}</p>
+          <p>{sessionSuccessPercent}</p>
           </Card.Text>
         </Card.Body>
       </Card>
