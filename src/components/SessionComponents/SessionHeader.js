2bd93c8d53c470b63bace1a356fe36d57339564c
Sarah <sarah.mahov@gmail.com>
2019-11-05 20:02:36 +0000

    remove time bug

--- src/components/SessionComponents/SessionHeader.js
+++ src/components/SessionComponents/SessionHeader.js
@@ -4,21 +4,21 @@ import { totalPracticeTimeFunction} from '../../services/overallCalculations';
  
 export default function SessionHeader({start_time, end_time}) {
 
-const dateString = () => {
-  return start_time.slice(0, 10);   
- }
+// const dateString = () => {
+//   return start_time.slice(0, 10);   
+//  }
 
- const sessionDate = dateString();
+//  const sessionDate = dateString();
 
- const practiceTime = totalPracticeTimeFunction([{
-   "start_time": start_time,
-   "end_time": end_time
- }])
+//  const practiceTime = totalPracticeTimeFunction([{
+//    "start_time": start_time,
+//    "end_time": end_time
+//  }])
 
   return (
     <div>
-      <p>Session Date: {sessionDate}</p>
-      <p>Length of Practice: {practiceTime} minutes</p>
+      {/* <p>Session Date: {sessionDate}</p>
+      <p>Length of Practice: {practiceTime} minutes</p> */}
     </div> 
   );
 }
