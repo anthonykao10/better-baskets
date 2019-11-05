7b993c80dbf7201479fc8d75d131c8b7a39e51a1
Andrew <andrew.j.hui@gmail.com>
2019-11-05 05:13:18 +0000

    making borders of green and red cards bolder

--- src/components/SessionComponents/RedStatCard.js
+++ src/components/SessionComponents/RedStatCard.js
@@ -10,7 +10,7 @@ export default function RedStatCard({name, statistic}) {
 
   return (
      <>
-      <Card border="danger" style={{ width: '10rem' }} className="statCard">
+      <Card border="danger" style={{ width: '10rem' }} className="statCard redStatCard">
         <Card.Header>{name}</Card.Header>
         <Card.Body>
           <Card.Title>{statistic}</Card.Title>
