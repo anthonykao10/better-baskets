850a0e63e8ee5730ebfd8ccef1d4372d2501bac2
anthony <anthonykao10@gmail.com>
2019-11-06 11:42:35 -0800

    add dynamic shot success/fail messages to ShotScreen

--- src/services/shotCalculations.js
+++ src/services/shotCalculations.js
@@ -9,3 +9,32 @@ export const shotArcDetermination = function(arcHeight) {
     return "Medium"
   }
 };
+
+export const successMessages = function() {
+  const messages = [
+    "Great Shot!",
+    "OOOOOoooo that was cleannnnnnn",
+    "motivational WIINNER WINNER CHICKEN DINNER",
+    "motivational SWISH!",
+    "motivational NICE SHOT!!",
+    "Acceptable shot",
+    "NICE ARC"
+  ];
+  const min = 0;
+  const max = messages.length - 1;
+  const idx =  Math.floor(Math.random() * (max - min + 1)) + min;
+  return messages[idx];
+}
+
+export const failureMessages = function() {
+  const messages = [
+    "Better luck next time!",
+    "better luck message 2",
+    "better luck message 3",
+    "That was 1.385° too high, and 3.683° too far left, with way too much spin." 
+  ];
+  const min = 0;
+  const max = messages.length - 1;
+  const idx =  Math.floor(Math.random() * (max - min + 1)) + min;
+  return messages[idx];
+}
